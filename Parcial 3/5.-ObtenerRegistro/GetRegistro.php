420fdc177fabf974f4b3653fe02131174edda69a
YoelRM <munozjavier541@gmail.com>
2022-11-16 18:07:13 -0600

    Act

---
+++ Parcial 3/5.-ObtenerRegistro/GetRegistro.php
@@ -0,0 +1,5 @@
+<?php
+    $registro["id"] = "244";
+    $registro["nombre"] = "Joel";
+    echo json_encode($registro);
+?>
---
+++ Parcial 3/5.-ObtenerRegistro/Peticion.js
@@ -0,0 +1,20 @@
+// Metodo con promesas
+//  document.getElementById("btnGenReg").addEventListener("click",function(){
+//     fetch('GetRegistro.php')
+//     .then(respuesta=>respuesta.json())
+//     .then(dato=>displayCampos(dato));
+// });
+
+//     function displayCampos(reg){
+//         document.getElementById("idEmpleado").value=reg.id;
+//         document.getElementById("nombreEmpleado").value=reg.Nombre;
+//     }
+
+    // metodo con Asyn Await
+    document.getElementById("btnGenReg").addEventListener("click",async()=>{
+    let respuesta = await fetch('GetRegistro.php')
+    let dato = await respuesta.json();
+    document.getElementById("nombre").value=dato.nombre;
+    document.getElementById("ocupacion").value=dato.ocupacion;
+    document.getElementById("NumTelefono").value=dato.NumeroTel;
+    });
